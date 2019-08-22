a703b315ae57a7afa8f34d905124ecdfda609c1d
Javed K3 <javedk3@chetu.com>
2019-08-22 23:27:13 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore}  from 'redux';
+
+//STORE       -> Globalizied State
+// Action     -> Increment
+
+const increment  = ()=>{
+    return {
+        type:'INCREMENT'
+    }
+}
+const decerement  = ()=>{
+    return {
+        type:'DECEREMENT'
+    }
+}
+//Resucer     -> 
+const counter  = (state=0,action)=>{
+switch(action.type){
+
+    case "INCREMENT" : return state+1
+    case "DECEREMENT"  : return state-1
+  
+}
+}
+
+let store  =  createStore(counter);
+store.subscribe(()=> console.log("store ----",store.getState()));
+
+store.dispatch(increment)
+//Dispatched  ->
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
